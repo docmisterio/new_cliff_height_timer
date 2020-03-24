7870e1a84f786adaec8f96c5a1906a694ed7b47c
Zach Becker <doc.misterio@gmail.com>
2020-03-23 19:49:02 -0500

    made new file for the button class, attempting to get things situated after the move, it's for sure broken right now. Commiting to make sure I have a place to come back to if I fuck it up even more

--- File.swift
+++ File.swift
@@ -1,9 +1,75 @@
-//
-//  File.swift
-//  newCliffHeightTimer
-//
-//  Created by Zach Becker on 3/23/20.
-//  Copyright © 2020 Zach Becker. All rights reserved.
-//
-
 import Foundation
+import UIKit
+
+class TheButton: UIButton {
+    private lazy var timerView = TimerView()
+    private lazy var timerState = TimerState()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            // since I already have an enum that handles state of the button wouldn't I want use that for the isHightlighted bool in UIButton?
+        }
+    }
+    
+    enum TimerState {
+        case ready
+        case running
+        case stopped
+    }
+    
+    @objc func buttonTapped() {
+                  switch timerState {
+                  case .ready:
+                      startTapped()
+                  case .running:
+                      stopTapped()
+                  case .stopped:
+                       resetTapped()
+                  }
+              }
+              
+              func startTapped() {
+                  timerState = .running
+                  print("start tapped")
+                  activeTimer?.invalidate()
+                  
+                  let startDate = Date()
+                  activeTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { _ in
+                      let currentDate = Date()
+                      let duration = currentDate.timeIntervalSince(startDate)
+                      
+                      let formattedDuration = self.labelFormatter.format(duration)
+                      
+                      self.timerView.timerLabel.text = formattedDuration
+                  })
+                  timerView.actionButton.setTitle("STOP", for: .normal)
+                  timerView.actionButton.setTitleColor(.white, for: .normal)
+                  timerView.actionButton.backgroundColor = .red
+              }
+              
+              func stopTapped() {
+                  timerState = .stopped
+                  print("stop tapped")
+                  activeTimer?.invalidate()
+                  
+                  timerView.actionButton.setTitle("RESET", for: .normal)
+                  timerView.actionButton.setTitleColor(.black, for: .normal)
+                  timerView.actionButton.backgroundColor = .white
+              }
+              
+              func resetTapped() {
+                  timerState = .ready
+                  print("reset tapped")
+                  
+                  timerView.actionButton.backgroundColor = .white
+                  timerView.actionButton.setTitle("START", for: .normal)
+                  timerView.actionButton.setTitleColor(.blue, for: .normal)
+                  timerView.timerLabel.text = "0.00"
+              }
+          }
+      }
+}
--- newCliffHeightTimer/ViewController.swift
+++ newCliffHeightTimer/ViewController.swift
@@ -2,82 +2,22 @@ import UIKit
 
 class ViewController: UIViewController {
     private lazy var timerView = TimerView()
+    private lazy var button = TheButton()
     private var activeTimer: Timer?
     private var timerState: TimerState = .ready
     private let labelFormatter = LabelFormatter()
     
-    enum TimerState {
-        case ready
-        case running
-        case stopped
-    }
-    
     override func loadView() {
         view = timerView
         timerView.installContraints()
-        timerView.actionButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-    }
-    
-    @objc func buttonTapped() {
-        switch timerState {
-        case .ready:
-            startTapped()
-        case .running:
-            stopTapped()
-        case .stopped:
-             resetTapped()
-        }
-    }
-    
-    func startTapped() {
-        timerState = .running
-        print("start tapped")
-        activeTimer?.invalidate()
-        
-        let startDate = Date()
-        activeTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { _ in
-            let currentDate = Date()
-            let duration = currentDate.timeIntervalSince(startDate)
-            
-            let formattedDuration = self.labelFormatter.format(duration)
-            
-            self.timerView.timerLabel.text = formattedDuration
-        })
-        timerView.actionButton.setTitle("STOP", for: .normal)
-        timerView.actionButton.setTitleColor(.white, for: .normal)
-        timerView.actionButton.backgroundColor = .red
-    }
-    
-    func stopTapped() {
-        timerState = .stopped
-        print("stop tapped")
-        activeTimer?.invalidate()
-        
-        timerView.actionButton.setTitle("RESET", for: .normal)
-        timerView.actionButton.setTitleColor(.black, for: .normal)
-        timerView.actionButton.backgroundColor = .white
-    }
-    
-    func resetTapped() {
-        timerState = .ready
-        print("reset tapped")
-        
-        timerView.actionButton.backgroundColor = .white
-        timerView.actionButton.setTitle("START", for: .normal)
-        timerView.actionButton.setTitleColor(.blue, for: .normal)
-        timerView.timerLabel.text = "0.00"
+        timerView.actionButton.addTarget(self, action: #selector(button.buttonTapped), for: .touchUpInside)
     }
 }
 
 // pad the button
+// make button same size no matter what state it's in
 // make the button press / buttonSubClass
 // organize the files / functions
 // add math labels for meter and feet, readjust the timer label up
 
-class TheButton: UIButton {
-    override var isHighlighted: Bool {
-        didSet {
-            // since I already have an enum that handles state of the button wouldn't I want use that for the isHightlighted bool in UIButton?
-        }
-    }
-}
+

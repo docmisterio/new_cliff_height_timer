39a239d2b3480a3333b6ad3892752d71baba6b0a
Zach Becker <doc.misterio@gmail.com>
2020-03-23 19:48:36 -0500

    made new file for the button class, attempting to get things situated after the move, it's for sure broken right now. Commiting to make sure I have a place to come back to if I fuck it up even more

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  newCliffHeightTimer
+//
+//  Created by Zach Becker on 3/23/20.
+//  Copyright © 2020 Zach Becker. All rights reserved.
+//
+
+import Foundation
---
+++ TheButton.swift
@@ -0,0 +1,9 @@
+//
+//  TheButton.swift
+//  newCliffHeightTimer
+//
+//  Created by Zach Becker on 3/23/20.
+//  Copyright © 2020 Zach Becker. All rights reserved.
+//
+
+import Foundation

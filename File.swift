500ae81fb8968d54f3ff6641feae1b9e394ba666
Zach Becker <doc.misterio@gmail.com>
2020-03-23 20:15:59 -0500

    made init in UIButton, added questions for things, still broken

--- File.swift
+++ File.swift
@@ -2,8 +2,14 @@ import Foundation
 import UIKit
 
 class TheButton: UIButton {
-    private lazy var timerView = TimerView()
-    private lazy var timerState = TimerState()
+    private var timerView = TimerView()
+    private var timerState = TimerState() // why is this happening when it's not happening to the other vars
+    private var activeTimer: Timer? // why doesn't this need to be in the init is it the optional? 
+    
+    required init(timerView: TimerView, timerState: TimerState) {
+        self.timerView = timerView
+        self.timerState = timerState
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -22,54 +28,52 @@ class TheButton: UIButton {
     }
     
     @objc func buttonTapped() {
-                  switch timerState {
-                  case .ready:
-                      startTapped()
-                  case .running:
-                      stopTapped()
-                  case .stopped:
-                       resetTapped()
-                  }
-              }
-              
-              func startTapped() {
-                  timerState = .running
-                  print("start tapped")
-                  activeTimer?.invalidate()
-                  
-                  let startDate = Date()
-                  activeTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { _ in
-                      let currentDate = Date()
-                      let duration = currentDate.timeIntervalSince(startDate)
-                      
-                      let formattedDuration = self.labelFormatter.format(duration)
-                      
-                      self.timerView.timerLabel.text = formattedDuration
-                  })
-                  timerView.actionButton.setTitle("STOP", for: .normal)
-                  timerView.actionButton.setTitleColor(.white, for: .normal)
-                  timerView.actionButton.backgroundColor = .red
-              }
-              
-              func stopTapped() {
-                  timerState = .stopped
-                  print("stop tapped")
-                  activeTimer?.invalidate()
-                  
-                  timerView.actionButton.setTitle("RESET", for: .normal)
-                  timerView.actionButton.setTitleColor(.black, for: .normal)
-                  timerView.actionButton.backgroundColor = .white
-              }
-              
-              func resetTapped() {
-                  timerState = .ready
-                  print("reset tapped")
-                  
-                  timerView.actionButton.backgroundColor = .white
-                  timerView.actionButton.setTitle("START", for: .normal)
-                  timerView.actionButton.setTitleColor(.blue, for: .normal)
-                  timerView.timerLabel.text = "0.00"
-              }
-          }
-      }
+        switch timerState {
+        case .ready:
+            startTapped()
+        case .running:
+            stopTapped()
+        case .stopped:
+            resetTapped()
+        }
+    }
+    
+    func startTapped() {
+        timerState = .running
+        print("start tapped")
+        activeTimer?.invalidate()
+        
+        let startDate = Date()
+        activeTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { _ in
+            let currentDate = Date()
+            let duration = currentDate.timeIntervalSince(startDate)
+            
+            let formattedDuration = self.labelFormatter.format(duration)
+            
+            self.timerView.timerLabel.text = formattedDuration
+        })
+        timerView.actionButton.setTitle("STOP", for: .normal)
+        timerView.actionButton.setTitleColor(.white, for: .normal)
+        timerView.actionButton.backgroundColor = .red
+    }
+    
+    func stopTapped() {
+        timerState = .stopped
+        print("stop tapped")
+        activeTimer?.invalidate()
+        
+        timerView.actionButton.setTitle("RESET", for: .normal)
+        timerView.actionButton.setTitleColor(.black, for: .normal)
+        timerView.actionButton.backgroundColor = .white
+    }
+    
+    func resetTapped() {
+        timerState = .ready
+        print("reset tapped")
+        
+        timerView.actionButton.backgroundColor = .white
+        timerView.actionButton.setTitle("START", for: .normal)
+        timerView.actionButton.setTitleColor(.blue, for: .normal)
+        timerView.timerLabel.text = "0.00"
+    }
 }
